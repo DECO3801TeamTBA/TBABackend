39600d2d419565ed1407085e88f8f0f52e934d07
John-Paul Smith <johnpaul.smith@gmail.com>
2020-10-11 15:42:18 +1000

    added POCO for QR request
    - there were two API attributes for POST /qr, just had to remove one
    - tested in Postman and seems to be working

---
+++ WanderListAPI/Utility/Poco/QRRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WanderListAPI.Utility.Poco
+{
+    public class QRRequest
+    {
+        public Guid QRCode { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
